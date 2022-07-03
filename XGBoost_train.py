19a331b17a6604d665a7674c2d0b48def8915f52
1 <2251054600@qq.com>
2022-07-03 19:18:22 +0800

    客户端训练,包括测试，谁运行试一下，感觉不对，我电脑跑不动

--- XGBoost_train.py
+++ XGBoost_train.py
@@ -10,7 +10,13 @@ import numpy
 import csv
 
 
-def client_train(File_Name,gi,hi,round):
+def calculate_f1(y, t):
+    y_result = [1. if i > 0.5 else 0. for i in y]
+    print('f1score for creditcard2 =', f1_score(t, y_result))
+    return y_result
+
+
+def client_train(File_Name, gi, hi, round):
     df = pd.read_csv(File_Name)
     X_train = df[df.columns[:-1].tolist()]
     y_train = df[df.columns[-1]]
@@ -18,7 +24,15 @@ def client_train(File_Name,gi,hi,round):
                   n_estimators=1  # 总共迭代次数，每进行一轮进行一次全局更新
                   , max_depth=4, min_child_weight=0.2, gamma=0.03, )
     # 得到y_hat
-    client_y_hat = FML.fit(X_train, y_train,gi,hi,round)
-    # 返回一阶导和二阶导
-    return [FML._grad(client_y_hat,y_train),FML._hess(client_y_hat,y_train)]
+    client_y_hat = FML.fit(X_train, y_train, gi, hi, round)
+    # test
+    te = pd.read_csv('Data_Check/Data_Test.csv')
+    X_test = te[te.columns[:-1].tolist()]
+    y_test = te[te.columns[-1]]
+    y_pred = FML.predict_raw(X_test)
+    y_pred_proba = FML.predict_prob(X_test)[:, 1]
+    f1_pred = calculate_f1(y_pred, y_test)
+    # 返回一阶导和二阶导、f1的值、roc
+    return [FML._grad(client_y_hat, y_train), FML._hess(client_y_hat, y_train), f1_pred,
+            roc_auc_score(y_test, y_pred_proba)]
     # FML.save_model('model/clFML-mode.model')
